f1c496c6c445b3a24d3254ff9b12653997f5623b
buscar <holmesayala9@gmail.com>
2018-03-20 19:46:59 -0500

    Estructura Basica

---
+++ Brais/App_Code/Datos/Medico/DBMedico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de DBMedico
+/// </summary>
+public class DBMedico
+{
+    public DBMedico()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
---
+++ Brais/App_Code/Datos/Usuario/DBUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de DBUsuario
+/// </summary>
+public class DBUsuario
+{
+    public DBUsuario()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
