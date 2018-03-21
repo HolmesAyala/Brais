fb67517c8699fa41da8a9fa9d35a437514b02e1c
buscar <holmesayala9@gmail.com>
2018-03-20 22:15:45 -0500

    Se agrega: Pagina Principal

---
+++ Brais/Controller/MPHeaderFooter.master.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class View_MPHeaderFooter : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //imprimirConsola("Inicio!");
+    }
+
+    protected void BTN_PaginaPrincipal_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BTN_SobreNosotros_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BTN_CrearCuenta_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BTN_Login_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BTN_Recargar_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("PaginaPrincipal.aspx");
+    }
+
+    protected void imprimirConsola(String mensaje)
+    {
+        Response.Write("<script>console.log('" + mensaje + "');</script>");
+    }
+
+}
---
+++ Brais/View/PaginaPrincipal.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/View/MPHeaderFooter.master" AutoEventWireup="true" CodeFile="~/Controller/PaginaPrincipal.aspx.cs" Inherits="View_PaginaPrincipal" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    
+    <style>
+        section{
+            margin: 10px 0;
+            height: 300px;
+        }
+
+        section h2{
+            text-align: center;
+            background-color: teal;
+            color: white;
+            border-radius: 5px;
+            padding: 5px;
+        }
+
+    </style>
+
+    <section>
+
+    </section>
+
+    <section>
+        <h2>Conoce a nuestros profesionales</h2>
+
+    </section>
+
+    <section>
+        <h2>Una breve informacion sobre nosotros</h2>
+
+    </section>
+
+</asp:Content>
+
