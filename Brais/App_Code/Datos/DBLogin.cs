5cefe3264142695bec2292734c6141a2585f32f2
Anggy Arguello <ajarguello0710@gmail.com>
2018-03-24 02:12:39 -0500

    Se creo el formulario Login

---
+++ Brais/App_Code/Datos/DBLogin.cs
@@ -0,0 +1,51 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de DBLogin
+/// </summary>
+public class DBLogin
+{
+    public DBLogin()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+
+    public DataTable Login(EUsuario informacion)
+    {
+        DataTable usuario = new DataTable();
+        //Se Utiliza la libreria Configuration para no hacer SQL Quemado y crear la conexion a la BD
+        NpgsqlConnection conexion = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+        try
+        {
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuario.login", conexion);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            dataAdapter.SelectCommand.Parameters.Add("_identificacion", NpgsqlDbType.Text).Value = informacion.Identificacion;
+            dataAdapter.SelectCommand.Parameters.Add("_contrasena", NpgsqlDbType.Text).Value = informacion.Password;
+
+            conexion.Open();
+            dataAdapter.Fill(usuario);
+        }
+        catch (Exception Ex)
+        {
+            throw Ex;
+        }
+        finally
+        {
+            if (conexion != null)
+            {
+                conexion.Close();
+            }
+        }
+        return usuario;
+    }
+
+}
---
+++ Brais/Controller/Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+
+public partial class View_Login : System.Web.UI.Page
+{
+
+    private String mensaje_usuario = "Escriba su usuario";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void B_Ingresar_Click(object sender, EventArgs e)
+    {
+        EUsuario datosUsuario= new EUsuario();
+
+        datosUsuario.Identificacion = Tx_Identificacion.Text.ToString();
+        datosUsuario.Password = Tx_contrasena.Text.ToString();
+
+        DBLogin traerUsuario = new DBLogin();
+
+        DataTable usuario = traerUsuario.Login(datosUsuario);
+
+        if (usuario.Rows.Count == 0)
+        {
+            L_Informacion.Text = "El usuario no existe";
+            Session["identificacion"] = null;
+        }
+        else
+        {
+            Session["identificacion"] = usuario.Rows[0]["identificacion"];
+            Session["contrasena"] = usuario.Rows[0]["contrasena"];
+            Response.Redirect("PaginaPrincipal.aspx");
+            //L_Informacion.Text = "Correcto! " + usuario.Rows[0]["usuario"] + usuario.Rows[0]["clave"] + usuario.Rows[0]["rol_id"];
+        }
+    }
+}
