835e6a3c7101fb759e39ad4db0ce1967c9f6de1b
edkeran <edgarkrejci12345@gmail.com>
2018-03-24 00:00:01 -0500

    Se Crearon Las Funciones Para Obtener El Mac y El IP En la clase Gestor MAC

--- Brais/App_Code/GestorMAC.cs
+++ Brais/App_Code/GestorMAC.cs
@@ -1,7 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Configuration;
 using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
 
 /// <summary>
 /// Descripción breve de GestorMAC
@@ -13,5 +22,45 @@ public class GestorMAC
         //
         // TODO: Agregar aquí la lógica del constructor
         //
+    }
+    public String mac()
+    {
+
+        NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+        String sMacAddress = string.Empty;
+
+        foreach (NetworkInterface adapter in nics)
+        {
+
+            if (sMacAddress == String.Empty) // only return MAC Address from first card    
+            {
+
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+
+                sMacAddress = adapter.GetPhysicalAddress().ToString();
+
+            }
+
+        }
+        return sMacAddress;
+
+    }
+
+
+    public String ip()
+    {
+        string IP4Address = String.Empty;
+
+        foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+        {
+            if (IPA.AddressFamily == AddressFamily.InterNetwork)
+            {
+                IP4Address = IPA.ToString();
+                break;
+            }
+        }
+
+        return IP4Address;
     }
 }
