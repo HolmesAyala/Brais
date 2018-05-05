56f82279a193bef05e2b9651f6d58cb982e09ca8
Anggy Arguello <ajarguello0710@gmail.com>
2018-05-04 19:57:44 -0500

    Se muestran los pacientes: - Agendados con el doctor. -En el bloc de notas esta agregada una función que mejoré y una vista, sin embargo adjunte el backup porque tambipen cambié una vista. - Falta verificar que al medico solo le muestre los que son del mismo día, cómo un informe de las citas (Que por cierto es un reporte) Nota: El manual de usuario esta hecho como en un 25%

--- Brais/Controller/Medico/VerPacientes.aspx.cs
+++ Brais/Controller/Medico/VerPacientes.aspx.cs
@@ -9,13 +9,13 @@ public partial class View_Medico_VerPacientes : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //obtenerDatosPaciente();
+        obtenerDatosPaciente();
     }
 
     protected void obtenerDatosPaciente()
     { 
         DBUsuario dBUsuario= new DBUsuario();
-        GV_Pacientes.DataSource = dBUsuario.obtenerPaciente((String)Session["identificacion_medico"]);
+        GV_Pacientes.DataSource = dBUsuario.obtenerPacientesAgendados((String)Session["identificacion_medico"]);
         GV_Pacientes.DataBind();
     }
 
@@ -23,7 +23,7 @@ public partial class View_Medico_VerPacientes : System.Web.UI.Page
     protected void GV_Pacientes_PageIndexChanging(object sender, GridViewPageEventArgs e)
     {
         GV_Pacientes.PageIndex = e.NewPageIndex;
-        //obtenerDatosPaciente();
+        obtenerDatosPaciente();
     }
 
     protected void BTN_Modificar_Historial_Click(object sender, EventArgs e)
