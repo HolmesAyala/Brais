56e58ea68e993d45e3d55f0aaface4b8404e0534
edgar krejci <edgarkrejci_bautista@hotmail.com>
2018-05-04 22:54:13 -0500

    Se añade el primer reporte!!!

---
+++ Brais/View/Administrador/Reporte_Medicos.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/View/Administrador/MPAdministrador.master" AutoEventWireup="true" CodeFile="Reporte_Medicos.aspx.cs" Inherits="View_Administrador_Reporte_Medicos" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.3500.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="contenido" Runat="Server">
+    <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="True" GroupTreeImagesFolderUrl="" Height="50px" ReportSourceID="CR_medicos" ToolbarImagesFolderUrl="" ToolPanelWidth="200px" Width="350px" />
+    <CR:CrystalReportSource ID="CR_medicos" runat="server">
+        <Report FileName="~\View\Administrador\Reportes\RpMedico.rpt">
+        </Report>
+    </CR:CrystalReportSource>
+</asp:Content>
+
---
+++ Brais/View/Administrador/Reporte_Medicos.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class View_Administrador_Reporte_Medicos : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            InfoMedicos reporte = ObtenerInforme();
+            CR_medicos.ReportDocument.SetDataSource(reporte);
+            CrystalReportViewer1.ReportSource = CR_medicos;
+        }catch(Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    protected InfoMedicos ObtenerInforme()
+    {
+        DataRow fila;
+        DataTable inf_medc = new DataTable();
+        InfoMedicos datos = new InfoMedicos();
+        inf_medc = datos.Tables["Medico"];
+        DBMedico dbme = new DBMedico();
+        DataTable intermedio = dbme.buscarMedico("");
+        for(int i = 0; i < intermedio.Rows.Count; i++)
+        {
+            fila = inf_medc.NewRow();
+            fila["identificacion"] = intermedio.Rows[i]["identificacion"].ToString();
+            fila["nombre"] = intermedio.Rows[i]["nombre"].ToString();
+            fila["apellido"] = intermedio.Rows[i]["apellido"].ToString();
+            fila["correo"] = intermedio.Rows[i]["correo"].ToString();
+            inf_medc.Rows.Add(fila);
+        }
+        return datos;
+
+
+    }
+
+
+}
