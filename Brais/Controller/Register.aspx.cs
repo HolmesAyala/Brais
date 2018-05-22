0104fa77f68cbb327ae9338a1f97823b4d98cc3a
edgar krejci <edgarkrejci_bautista@hotmail.com>
2018-05-22 15:45:34 -0500

    Correccion Validacion EPS en register

--- Brais/Controller/Register.aspx.cs
+++ Brais/Controller/Register.aspx.cs
@@ -23,9 +23,50 @@ public partial class View_Default : System.Web.UI.Page
     {
         EUsuario user = new EUsuario();
         ClientScriptManager cm = this.ClientScript;
-        if (int.Parse(eps.SelectedItem.Value.ToString())== 2)
+        if (Tipo.SelectedValue == "1")
         {
-            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Seleccione una EPS')</script>;");
+            if (int.Parse(eps.SelectedItem.Value.ToString()) == 0)
+            {
+                cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Seleccione una EPS')</script>;");
+            }
+            else
+            {
+                 if (TB_pasword.Text == TB_password2.Text)
+                {
+                    user.Apellido = TB_lastName.Text.ToString();
+                    user.Correo = TB_email.Text.ToString();
+                    user.Identificacion = TB_id.Text.ToString();
+                    user.Nombre = TB_name.Text.ToString();
+                    user.Password = TB_pasword.Text.ToString();
+                    user.Tipo_afiliacion = int.Parse(Tipo.SelectedValue);
+                    user.Tipo_id = int.Parse(DropDownList1.SelectedValue);
+                    user.Fecha = TB_date.Text.ToString();
+                    user.IdEps = int.Parse(eps.SelectedItem.Value.ToString());
+                    DBUsuario bd = new DBUsuario();
+                    DataTable error;
+                    error = bd.CrearUsuario(user);
+                    if (error.Rows.Count == 0)
+                    {
+                        cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Se Ha Creado Usuario Exitosamente');</script>");
+                        Response.Redirect("PaginaPrincipal.aspx");
+                    }
+                    else if (error.Rows[0]["error"].ToString() == "error")
+                    {
+                        //fallo
+                        cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('El correo ingresado ya existe');</script>");
+                    }
+                    else
+                    {
+                        cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Se Ha Creado Usuario Exitosamente');</script>");
+                        Response.Redirect("PaginaPrincipal.aspx");
+                    }
+                }
+                else
+                {
+                    //ESTA MAL LA CLAVE
+                    cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Las Contraseñas Dadas No Coinciden');</script>");
+                }
+            }
         }else if (TB_pasword.Text == TB_password2.Text)
         {
             user.Apellido = TB_lastName.Text.ToString();
