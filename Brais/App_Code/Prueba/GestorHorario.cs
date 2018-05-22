31b1e7b69e7606efa0c25007c35c5b1d38232c93
buscar <holmesayala9@gmail.com>
2018-05-21 23:41:23 -0500

    Correccion validacion de cruces de horarios

--- Brais/App_Code/Prueba/GestorHorario.cs
+++ Brais/App_Code/Prueba/GestorHorario.cs
@@ -47,8 +47,8 @@ public class GestorHorario
                 foreach (Rango r in d.Rangos)
                 {
                     if ( (DateTime.Parse(r.Inicio) == DateTime.Parse(rango.Inicio) && DateTime.Parse(r.Fin) == DateTime.Parse(rango.Fin)) ||
-                         (DateTime.Parse(r.Inicio) < DateTime.Parse(rango.Inicio) && DateTime.Parse(rango.Inicio) < DateTime.Parse(r.Fin)) ||
-                         (DateTime.Parse(r.Inicio) < DateTime.Parse(rango.Fin) && DateTime.Parse(rango.Fin) < DateTime.Parse(r.Fin))
+                         (DateTime.Parse(rango.Inicio) < DateTime.Parse(r.Inicio) && DateTime.Parse(r.Inicio) < DateTime.Parse(rango.Fin)) ||
+                         (DateTime.Parse(rango.Inicio) < DateTime.Parse(r.Fin) && DateTime.Parse(r.Fin) < DateTime.Parse(rango.Fin))
                         )
                     {
                         return false;
